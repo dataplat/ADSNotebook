d87af0e6ddcbb1ded5fa4111d79ed6858609b76a
Rob Sewell <mrrobsewell@outlook.com>
2019-11-04 00:50:28 +0000

    load the module please in teh help tests

--- tests/Help.Tests.ps1
+++ tests/Help.Tests.ps1
@@ -29,7 +29,7 @@ if ([System.Version]::TryParse($leaf, [ref]$parsedVersion)) {
 }
 # for VSTS build agent
 elseif ($leaf -eq 's') {
-    $ModuleName = 'ADSNotebook'
+    $ModuleName = $Env:Build_Repository_Name.Split('/')[1]
 }
 else {
     $ModuleName = $leaf
